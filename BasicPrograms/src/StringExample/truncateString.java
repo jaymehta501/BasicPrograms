50673ea77357182fa0c89817adf4c9b5231108ed
= <jaymehta501@gmail.com>
2018-06-21 01:25:47 -0700

    truncateString

---
+++ BasicPrograms/src/StringExample/truncateString.java
@@ -0,0 +1,30 @@
+package StringExample;
+
+public class truncateString {
+
+	public static void main(String[] args) {
+		truncateStringWithNumbers("aaaaaaabbbbbcccddddddeeeefffgga");
+	}
+	
+	public static void truncateStringWithNumbers(String usrString) {
+
+		int temp = 1;
+		String result = "";
+
+		for (int i = 0; i < usrString.length(); i++) {
+
+			for (int p = i + 1; p < usrString.length(); p++) {
+
+				if (usrString.charAt(p) == usrString.charAt(i)) {
+					temp++;
+					i++;
+				} else {
+					break;
+				}
+			}			
+				result = result + usrString.charAt(i) + Integer.toString(temp) + " ";
+				temp = 1;
+		}
+		System.out.println(result);
+	}
+}
