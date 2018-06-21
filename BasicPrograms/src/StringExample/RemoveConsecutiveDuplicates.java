3f0d50f49e85a1ba15fb9c1c9c8d2d25d4a94adf
= <jaymehta501@gmail.com>
2018-06-21 00:15:59 -0700

    removeConsecutiveDuplicates

--- BasicPrograms/src/StringExample/RemoveConsecutiveDuplicates.java
+++ BasicPrograms/src/StringExample/RemoveConsecutiveDuplicates.java
@@ -6,30 +6,29 @@ public class RemoveConsecutiveDuplicates {
 		// TODO Auto-generated method stub
 		String usr1 = "aaabcdeeefghiiijklllmnnn";
 		int num1 = 3;
-		
+
 		removeConsecutiveDuplicates(usr1, num1);
 	}
 
-	
-	public static void removeConsecutiveDuplicates(String usr1, int num1){
-		
-		for (int i =0; i < usr1.length(); i++){
-			
+	public static void removeConsecutiveDuplicates(String usr1, int num1) {
+
+		for (int i = 0; i < usr1.length(); i++) {
+
 			char temp = usr1.charAt(i);
-			
-			if (i != usr1.length() && usr1.charAt(i+1) == temp){
-				
-				if(i != usr1.length() && usr1.charAt(i+2) == temp){
-					
-					i = i+2;
+
+			if (i != usr1.length() && usr1.charAt(i + 1) == temp) {
+
+				if (i != usr1.length() && usr1.charAt(i + 2) == temp) {
+
+					i = i + 2;
 				}
-				
+
 			} else {
-				
+
 				System.out.print(usr1.charAt(i));
 			}
-			
+
 		}
-		
+
 	}
 }
